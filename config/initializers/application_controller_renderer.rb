de21585f09b8d94c7d5d5dde10f46fb347208cc0
naoki0925 <naoking250912@iclod.com>
2021-02-04 14:21:36 +0000

    [4-3start/finish]画像投稿機能を作ろう

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7c85cc30c814d486f500e1d16f28dbde79c56f8e7acad32ee89877d191ce742450de1f4bf4f956391896203b00fb003cbb222609ac00409a129d09113834143b'
